32e35753d2556f14f9af9d8ec4d7c30a25c65210
Rommel Gonzalez <abril_17.92@hotmail.com>
2022-07-27 08:11:13 -0500

    Añadiendo archivos iniciales del proyecto

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,14 @@
+const nombre = 'Monitor 20 pulgadas';
+const precio = 200;
+const disponible = true;
+
+// Un objeto agrupa todo en una sola variable...
+
+// Objeto Literal
+const product = {
+    nombre : 'Monitor 20 pulgadas',
+    precio : 200,
+    disponible : true
+}
+
+console.log(product);
---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,8 @@
+const product = {
+    nombre : 'Monitor 20 pulgadas',
+    precio : 200,
+    disponible : true
+}
+console.log(product);
+console.log(product.nombre);
+console.log(product['nombre']);
